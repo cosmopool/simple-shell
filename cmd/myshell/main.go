a6a5d1c76244e844e72f734a17a0603d3e8fdab6
Kaio Delphino <github@kaiodelphino.com>
2024-06-15 10:51:16 -0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	rawInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		os.Stderr.Write([]byte(fmt.Sprint(err)))
+		os.Exit(1)
+	}
+
+	command := strings.Trim(rawInput, "\n")
+	for {
+		if command == "exit" {
+			os.Exit(0)
+		}
+		result := fmt.Sprintf("%s: command not found", command)
+		os.Stderr.Write([]byte(result))
+	}
 }
