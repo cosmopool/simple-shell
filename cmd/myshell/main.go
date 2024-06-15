409bb490f0895efa3a10d62751f92928ef7e65ec
Kaio Delphino <github@kaiodelphino.com>
2024-06-15 11:31:29 -0300

    fix: exit with no arguments

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -15,6 +15,12 @@ func getArgumentsFromCommand(input string) (string, []string) {
 }
 
 func parseExitCommand(args []string) {
+	var exitCode int
+
+	if len(args) == 0 {
+		os.Exit(0)
+	}
+
 	exitCode, err := strconv.Atoi(args[0])
 	if err != nil {
 		fmt.Fprint(os.Stderr, err)
